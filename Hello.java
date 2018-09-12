67bb5a998c72c7564026b4f19c1bbbcab1c202f3
David Falana <falanadavid@gmail.com>
2018-09-12 14:51:27 -0400

    code formatting

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
-public static void main (String{}args) {
-System.out.printin("Hello, World!");
-}
+	public static void main (String{}args) {
+		System.out.printin("Hello, World!");
+	}
 }
